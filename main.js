e342f0e78e1c5a1cd079864746150528cf812feb
RyanRosales <60956366+RyanRosales@users.noreply.github.com>
2020-06-13 01:41:29 -0700

    Create Homepage

---
+++ main.js
@@ -0,0 +1,106 @@
+// Crunchyroll
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if($(window).scrollTop()>300){
+    $('.crunchy-top-bttn').fadeIn(250);
+}
+else{
+    $('.crunchy-top-bttn').fadeOut(250);
+}
+  });
+  $('.crunchy-top-bttn').click(function(){
+$('html,body').animate(
+    {scrollTop:0},400
+);
+  });
+});
+
+
+// Discord
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if($(window).scrollTop()>300){
+    $('.discord-top-bttn').fadeIn(250);
+}
+else{
+    $('.discord-top-bttn').fadeOut(250);
+}
+  });
+  $('.discord-top-bttn').click(function(){
+$('html,body').animate(
+    {scrollTop:0},400
+);
+  });
+});
+
+
+// Travel
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if($(window).scrollTop()>300){
+    $('.travel-top-bttn').fadeIn(250);
+}
+else{
+    $('.travel-top-bttn').fadeOut(250);
+}
+  });
+  $('.travel-top-bttn').click(function(){
+$('html,body').animate(
+    {scrollTop:0},400
+);
+  });
+});
+
+
+// Client
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if($(window).scrollTop()>300){
+    $('.client-top-bttn').fadeIn(250);
+}
+else{
+    $('.client-top-bttn').fadeOut(250);
+}
+  });
+  $('.client-top-bttn').click(function(){
+$('html,body').animate(
+    {scrollTop:0},400
+);
+  });
+});
+
+
+// Interaction
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if($(window).scrollTop()>300){
+    $('.interaction-top-bttn').fadeIn(250);
+}
+else{
+    $('.interaction-top-bttn').fadeOut(250);
+}
+  });
+  $('.interaction-top-bttn').click(function(){
+$('html,body').animate(
+    {scrollTop:0},400
+);
+  });
+});
+
+
+// esports
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if($(window).scrollTop()>300){
+    $('.esports-top-bttn').fadeIn(250);
+}
+else{
+    $('.esports-top-bttn').fadeOut(250);
+}
+  });
+  $('.esports-top-bttn').click(function(){
+$('html,body').animate(
+    {scrollTop:0},400
+);
+  });
+});
